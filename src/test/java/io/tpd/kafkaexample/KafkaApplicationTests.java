9ed63659454ae62873a37b8b43bd755d6da3dba0
jrfilocao <bla@email.com>
2022-04-10 23:43:01 +0200

    Add initial files

---
+++ src/test/java/io/tpd/kafkaexample/KafkaApplicationTests.java
@@ -0,0 +1,12 @@
+package io.tpd.kafkaexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class KafkaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+}
