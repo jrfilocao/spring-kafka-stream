fc2d80987c878c3a875e6d52ad093a664910ac7f
jrfilocao <bla@email.com>
2022-04-11 00:06:45 +0200

    Add reference to possible tests

--- src/test/java/io/tpd/kafkaexample/KafkaApplicationTests.java
+++ src/test/java/io/tpd/kafkaexample/KafkaApplicationTests.java
@@ -3,6 +3,9 @@ package io.tpd.kafkaexample;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * If more time, then tests similar to https://www.baeldung.com/spring-boot-kafka-streams
+ */
 @SpringBootTest
 public class KafkaApplicationTests {
 
